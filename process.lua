6187fa2de2b42718d736275634514041be4c3b1e
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2024-08-30 12:24:54 +0200

    add process.lua to easily add songs

---
+++ process.lua
@@ -0,0 +1,25 @@
+local fileList = fs.list("files")
+print("Please enter the base URL")
+local baseURL = read()
+local data = {}
+for i,o in pairs(fileList) do
+  print("File: " .. o)
+  data[i] = {}
+  data[i].file = textutils.urlEncode(baseURL .. o)
+  local h = http.get(textutils.urlEncode(baseURL .. o))
+  if h then
+    data[i].lyrics = textutils.urlEncode(baseURL .. o:sub(1, -6) .. ".lrc")
+    h.close()
+  end
+  data[i].type = "song"
+  
+  print("What is the artist?")
+  data[i].author = read()
+  print("What is the song name?")
+  data[i].name = read()
+end
+
+local file = fs.open("data.json", "w")
+file.write(textutils.serializeJSON(data))
+file.close()
+print("Done!")
